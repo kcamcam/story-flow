296069aca8a1f78709ca9a6944f62b1835b13cbe
izconcept <IzKevin@Kevins-MacBook-Pro.local>
2017-01-21 16:38:07 -0500

    Improved playback
    Added looping + click to skip functionality
    
    Added contributor  information under video

--- load.js
+++ load.js
@@ -6,7 +6,8 @@ function loadPage(storyName) {
         if(data[storyName]) {
             $(".mainTitle").html(storyName);
             payload = data[storyName];
-            $(".container").append('<video id="my-video" height="640" width="360"autoplay onended="cycleVideos();"> <source id="ss"  src="'+data[storyName][0]["url"]+'" type="video/mp4"></video>')
+            $(".container").append('<video id="my-video" height="640" width="360" autoplay onended="cycleVideos();"> <source id="ss"  src="'+payload[0]["url"]+'" type="video/mp4"></video>')
+            $(".container").append("<div id='underVideoTxt'><img id='underImage' src='"+payload[0]['profile_pic']+"'><span id='underText'>"+payload[0]['full_name']+"</span></div>");
         } else {
             
         }
@@ -15,10 +16,18 @@ function loadPage(storyName) {
 
 function cycleVideos() {
     var videoElement = $("#my-video");
-    if(videoIndex < payload.length) {
-        videoElement.attr("src", payload[videoIndex]["url"]);
-        videoElement.get(0).play();
-        videoIndex++;
-        console.log(videoIndex);
+    if(videoIndex >= payload.length) {
+        videoIndex = 0;
     }
+    videoElement.attr("src", payload[videoIndex]["url"]);
+    videoElement.get(0).play();
+    $("#underImage").attr("src", payload[videoIndex]['profile_pic']);
+    $("#underText").text(payload[videoIndex]['full_name']);
+    videoIndex++;
 }
+
+$(document).ready(function() {  
+    $("body").on("click", "video", function() {
+        cycleVideos();
+    })
+})
